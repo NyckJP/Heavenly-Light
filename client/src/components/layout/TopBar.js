f9672b9b34c9cdac7af84b7c999e6f69ed4e0702
Nyck Jean-Philippe <nyckjeanphilippe@gmail.com>
2024-09-10 11:29:36 -0400

    responsive top bar

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SignOutButton from "../authentication/SignOutButton";
 
 const TopBar = ({ user }) => {
   const unauthenticatedListItems = [
-    <li key="sign-in">
-      <Link to="/user-sessions/new">Sign In</Link>
-    </li>,
-    <li key="sign-up">
-      <Link to="/users/new" className="button">
-        Sign Up
+    <li className="nav-link" key="products">
+      <Link>
+        Products
       </Link>
     </li>,
+    <li className="nav-link" key="about">
+      <Link>
+        About
+      </Link>
+    </li>,
+    <li className="nav-link" key="contact">
+      <Link>
+        Contact
+      </Link>
+    </li>,
+    <li key="Basket">
+      <Link>
+        <i className="fa-solid fa-basket-shopping" />
+      </Link>
+    </li>,
+    // <li key="sign-in">
+    //   <Link to="/user-sessions/new">Sign In</Link>
+    // </li>,
+    // <li key="sign-up">
+    //   <Link to="/users/new" className="button">
+    //     Sign Up
+    //   </Link>
+    // </li>
   ];
 
   const authenticatedListItems = [
@@ -21,18 +41,16 @@ const TopBar = ({ user }) => {
   ];
 
   return (
-    <div className="top-bar">
-      <div className="top-bar-left">
-        <ul className="menu">
-          <li className="menu-text">App</li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
+    <div className="top-bar sticky">
+      <div className="hide-on-large-screens">
+        <Link>
+          <i className="fa-solid fa-bars" />
+        </Link>
       </div>
-      <div className="top-bar-right">
-        <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
-      </div>
+      <ul className="menu">
+        <li className="menu-text">Heavenly Light</li>
+      </ul>
+      <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
     </div>
   );
 };
