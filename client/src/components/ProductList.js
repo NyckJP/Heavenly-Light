8054b8ce2d09de151445f6059a4c22672efd4ef4
Nyck Jean-Philippe <nyckjeanphilippe@gmail.com>
2024-10-01 23:10:33 -0400

    initial filter function and styling

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -5,12 +5,19 @@ const ProductList = () => {
     const [category, setCategory] = useState("All Products")
     const [products, setProducts] = useState([])
 
+    const setFilter = (allProducts, newCategory) => {
+        if(newCategory == "All Products")
+            return allProducts
+        const newList = allProducts.filter(product => product.category == newCategory)
+        return newList
+    }
+
     const getProducts = async () => {
         try {
             const response = await fetch(`/api/v1/products`)
             const parsedResponse = await response.json()
-            console.log(parsedResponse.products)
-            setProducts(parsedResponse.products)
+            const filteredList = setFilter(parsedResponse.products, category)
+            setProducts(filteredList)
         } catch (error) {
             console.error(`Error in product list fetch: ${error}`)
         }
@@ -18,7 +25,7 @@ const ProductList = () => {
 
     useEffect(() => {
         getProducts()
-    }, [])
+    }, [category])
 
     const productList = products.map(product => {
         return <ProductTile imageUrl={product.imageUrl} name={product.name} price={product.price} />
@@ -26,7 +33,22 @@ const ProductList = () => {
 
     return (
         <div className="grid-container products" id="products">
-            <h1>{category}</h1>
+            <div className="products-header">
+                <h1>{category}</h1>
+                <div className="category-buttons">
+                    <input type="radio" id="All" name="filter" onChange={() => setCategory("All Products")}/>
+                    <label htmlFor="All" className="category-button">All</label>
+
+                    <input type="radio" id="Shirts" name="filter" onChange={() => setCategory("shirts")}/>
+                    <label htmlFor="Shirts" className="category-button">Shirts</label>
+
+                    <input type="radio" id="Mugs" name="filter" onChange={() => setCategory("mugs")}/>
+                    <label htmlFor="Mugs" className="category-button">Mugs</label>
+
+                    <input type="radio" id="Crochet" name="filter" onChange={() => setCategory("crochet")}/>
+                    <label htmlFor="Crochet" className="category-button">Crochet</label>
+                </div>
+            </div>
             <hr />
             <div className="grid-x grid-margin-x grid-margin-y small-up-2 medium-up-2 large-up-4">
                 {productList}
